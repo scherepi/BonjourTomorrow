f870d02c46e9c262d5112a448b5e4aeffa884a5c
scherepi <scherejoaquin@gmail.com>
2025-02-11 14:34:26 -0600

    Still working on parsing.

--- BonjourClasses/ProgressHandler.cs
+++ BonjourClasses/ProgressHandler.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BonjourClasses
 {
     public class ProgressHandler
     {
-        public Dictionary<String, int> topicProgress;
+        public Dictionary<String, (int, int)> topicProgress;
         public Dictionary<String, bool> unlocks;
 
-        public ProgressHandler(String progressFile) { 
-            
+        public ProgressHandler(String progressFile) {
+            JsonDocument jsonProgress = JsonSerializer.SerializeToDocument(progressFile);
+            topicProgress = new Dictionary<String, (int, int)>();
+            foreach (JsonElement topic in jsonProgress.RootElement.GetProperty("topicProgress").EnumerateArray())
+            {
+                topicProgress.Add(topic.Name, (topic.GetProperty.));
+            }
         }
     }
 }
